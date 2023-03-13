8517a8423872a34a3129af58044182fd0777cfc1
ArrayLeks <lordleks@gmail.com>
2023-03-13 17:22:55 +0300

    Create APIObject.cs

---
+++ Проектирование на языке С#/Управление ресурсами/APIObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        int field;
+
+        public APIObject(int number)
+        {
+            field = number;
+            MagicAPI.Allocate(number);
+        }
+        
+        private bool disposedValue;
+
+        ~APIObject()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+
+                }
+                MagicAPI.Free(field);
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
